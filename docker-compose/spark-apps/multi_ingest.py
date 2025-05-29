f828e76b4c9d8bf3fc48cfc12c36344ce2cb5b13
diecccoo <dconti@gmail.com>
2025-05-29 16:02:33 +0200

    deleted

--- docker-compose/spark-apps/multi_ingest.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-with open("queries.txt", "r") as f:
-    queries = [line.strip() for line in f if line.strip()]
-
-for topic in queries:
-    print(f"Ingesting topic: {topic}")
-    subprocess.run(["python", "europeana_ingest.py", topic])
