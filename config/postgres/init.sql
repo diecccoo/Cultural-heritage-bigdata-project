9c2a1cb665b70d0a88f65815d3958bb6b7102ce2
silviabortoluzzi <bortoluzzisilvia02@gmail.com>
2025-07-01 19:47:50 +0200

    changed schema

---
+++ config/postgres/init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS europeana_items (
+    guid TEXT PRIMARY KEY,
+    title TEXT,
+    creator TEXT,
+    image_url TEXT,
+    timestamp_created TEXT,
+    provider TEXT,
+    description TEXT,
+    subject TEXT,
+    language TEXT,
+    type TEXT,
+    format TEXT,
+    rights TEXT,
+    dataProvider TEXT,
+    isShownAt TEXT,
+    isShownBy TEXT,
+    edm_rights TEXT,
+    raw_json JSONB
+);
+
+CREATE TABLE IF NOT EXISTS ugc_annotations (
+    id SERIAL PRIMARY KEY,
+    object_guid TEXT REFERENCES europeana_items(guid),
+    user_id TEXT,
+    comment TEXT,
+    tags TEXT[],
+    ugc_timestamp TIMESTAMP,
+    location TEXT
+);
